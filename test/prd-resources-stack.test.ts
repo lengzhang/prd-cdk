f51c3c035f9468fae3fff6eaf1a3f577eb938757
lengzhang <lengzhang@live.com>
2023-10-28 00:19:26 -0700

    cleanup and add stack for prd-resources

--- test/prd-resources-stack.test.ts
+++ test/prd-resources-stack.test.ts
@@ -1,26 +1,26 @@
 import * as cdk from "aws-cdk-lib";
 import { Template, Match } from "aws-cdk-lib/assertions";
 
-import HelloCdkStack from "../lib/hello-cdk-stack";
+import PrdResourcesStack from "../lib/prd-resources-stack";
 
 const env: cdk.Environment = {
   account: process.env.CDK_DEPLOY_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT,
   region: process.env.CDK_DEPLOY_REGION || process.env.CDK_DEFAULT_REGION,
 };
 
-describe("HelloCdkStack", () => {
-  const getHelloCdkStack = () => {
+describe("PrdResourcesStack", () => {
+  const getPrdResourcesStack = () => {
     const app = new cdk.App();
 
-    const stack = new HelloCdkStack(app, "test-stack-name", {
+    const stack = new PrdResourcesStack(app, "test-stack-name", {
       env,
-      bucketName: "test-bucket-name",
+      reportsBucketName: "test-bucket-name",
     });
 
     return stack;
   };
   test("matches the snapshot", () => {
-    const stack = getHelloCdkStack();
+    const stack = getPrdResourcesStack();
 
     expect(stack.stackName).toBe("test-stack-name");
 
@@ -29,7 +29,7 @@ describe("HelloCdkStack", () => {
   });
 
   test("can be auto deleted", () => {
-    const stack = getHelloCdkStack();
+    const stack = getPrdResourcesStack();
     const template = Template.fromStack(stack);
     template.hasResource("AWS::S3::Bucket", {
       DeletionPolicy: "Delete",
@@ -42,7 +42,7 @@ describe("HelloCdkStack", () => {
   });
 
   test("should have versioning", () => {
-    const stack = getHelloCdkStack();
+    const stack = getPrdResourcesStack();
     const template = Template.fromStack(stack);
     template.hasResourceProperties("AWS::S3::Bucket", {
       VersioningConfiguration: Match.objectEquals({
