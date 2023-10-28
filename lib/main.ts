bab6b921680fb0a69330b413c4875b32dd5f9c70
lengzhang <lengzhang@live.com>
2023-10-28 09:24:03 -0700

    update with stage env

--- lib/main.ts
+++ lib/main.ts
@@ -3,15 +3,19 @@ import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import PrdResourcesStack from "./prd-resources-stack";
 
+import { Environment } from "./types";
+
 const appName = process.env.npm_package_name;
-const env: cdk.Environment = {
+const env: Environment = {
   account: process.env.CDK_DEPLOY_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT,
   region: process.env.CDK_DEPLOY_REGION || process.env.CDK_DEFAULT_REGION,
+  stage: process.env.NODE_ENV || "prod",
 };
 
 function main() {
+  console.log(env);
   const app = new cdk.App();
-  const namePrefix = `${appName}-${env.account}-${env.region}`;
+  const namePrefix = `${env.stage}-${appName}-${env.account}-${env.region}`;
 
   const prdResourcesStackName = `${namePrefix}-resources-stack`;
   const prdResourcesBucketName = `${namePrefix}-reports-bucket`;
