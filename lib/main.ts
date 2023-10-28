ff78d722cf1d09fd8c8e137f0b7e9c5a97738bfe
lengzhang <lengzhang@live.com>
2023-10-28 00:43:52 -0700

    ts configure updated

--- lib/main.ts
+++ lib/main.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
-import PrdResourcesStack from "../lib/prd-resources-stack";
+import PrdResourcesStack from "./prd-resources-stack";
 
 const appName = process.env.npm_package_name;
 const env: cdk.Environment = {
