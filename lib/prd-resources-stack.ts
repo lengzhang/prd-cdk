6bfbfe5373c2fe755291453b18b66285a2aa3567
lengzhang <lengzhang@live.com>
2023-11-01 01:09:45 -0700

    [feat] add s3 cors rule

--- lib/prd-resources-stack.ts
+++ lib/prd-resources-stack.ts
@@ -1,23 +1,41 @@
 import * as cdk from "aws-cdk-lib";
+import * as s3 from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 
+import { Environment } from "./types";
+
 interface PrdResourcesStackProps extends cdk.StackProps {
+  env: Environment;
   reportsBucketName: string;
 }
 
 class PrdResourcesStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: PrdResourcesStackProps) {
     super(scope, id, props);
-
-    this.createPrdReportsBucket(props.reportsBucketName);
+    this.createPrdReportsBucket(props.reportsBucketName, props.env);
   }
 
-  createPrdReportsBucket(reportsBucketName: string) {
-    new cdk.aws_s3.Bucket(this, reportsBucketName, {
+  createPrdReportsBucket(reportsBucketName: string, env: Environment) {
+    const cors: s3.CorsRule[] = [];
+    const corsRule: s3.CorsRule = {
+      allowedMethods: [s3.HttpMethods.GET, s3.HttpMethods.PUT],
+      allowedOrigins: [],
+      allowedHeaders: ["*"],
+    };
+    if (env.stage === "prod" && process.env.ORIGIN) {
+      corsRule.allowedOrigins.push(process.env.ORIGIN);
+      cors.push(corsRule);
+    } else if (env.stage === "dev") {
+      corsRule.allowedOrigins.push("*");
+      cors.push(corsRule);
+    }
+
+    new s3.Bucket(this, reportsBucketName, {
       bucketName: reportsBucketName,
       versioned: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
+      cors,
     });
   }
 }
